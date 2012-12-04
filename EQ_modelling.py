b5976f97701f956bae16ddaf1463d4e58713dc76
Marius <marius.kriegerowski@zmaw.de>
2012-12-04 15:24:06 +0100

    lat_lon_from_dist_azi added New parameters added accordingly

--- EQ_modelling.py
+++ EQ_modelling.py
@@ -3,6 +3,23 @@ from pyrocko import util, pile_viewer
 from tunguska import gfdb, receiver, seismosizer, source, orthodrome 
 from tunguska.phase import Taper, Timing
 from numpy import array, pi, savetxt, complex, savetxt, prod, ones
+from math import acos, cos, asin, sin, pi, radians, degrees
+
+def lat_lon_from_dist_azi(olat, olon, dist, azim):
+    '''
+    :param olat: Latitude of origin, type: float
+    :param olon: Longitude of otigin, type: float
+    :param dist: Distance from origin, type: float
+    :param azim: Azimuth from origin, type: float
+    :return lat, lon: new latitude and longitude 
+    '''
+    rE=6371000.785
+    olat=radians(olat); olon=radians(olon); azi=radians(azim)
+    b=dist/rE
+    a=acos(cos(b)*cos(pi/2-olat)+sin(pi/2-olat)*sin(b)*cos(azi))
+    B=asin(sin(b)*sin(azi)/sin(a))
+    return 90-degrees(a), degrees(B)+degrees(olon)
+
 
 class STS2:
     ''' Apply the STS2's transfer function which is deduced from the 
@@ -10,7 +27,6 @@ poles, zeros and gain of the transfer tunction. The Green's function database (g
 Two gfdbs are needed.
 Three synthetic seismograms of an STS2 seismometer will be the result
 '''
-
     def evaluate(self,freqs):
         
         # transform the frequency to angular frequency.
@@ -29,6 +45,7 @@ Three synthetic seismograms of an STS2 seismometer will be the result
         for i_p in Poles:
             a /= w-i_p
         return a
+
 
 class extendedSnuffling(Snuffling):
     def __init__(self):
@@ -57,16 +74,24 @@ class ParaEditCp_TF_GTTG(extendedSnuffling):
         # Give the snuffling a name:
         self.set_name('EQmodelling')
        
-        self.db = gfdb.Gfdb('/scratch/local1/auto_Rotate_Test/gfdb_sediment/db')
+        #self.db = gfdb.Gfdb('/scratch/local1/auto_Rotate_Test/gfdb_sediment/db')
+        self.db = gfdb.Gfdb('/scratch/local1/gfdb_sediment_cut/db')
         
         # Add scrollbars of the parameters that you desire to adjust.
         # 1st argument: Description that appears within the snuffling.
         # 2nd argument: Name of parameter as used in the following code.
         # 3rd-5th argument: default, start, stop.
+        gfdb_maxrange=self.db.nx*self.db.dx-self.db.dx
+        gfdb_minrange=self.db.firstx
+
         self.add_parameter(Param('Source Depth [m]','source_depth', self.db.firstz, self.db.firstz, self.db.nz*self.db.dz-self.db.dz))
-        self.add_parameter(Param('Distance North [m]','d_north', self.db.firstx, self.db.firstx, self.db.nx*self.db.dx-self.db.dx))
-        self.add_parameter(Param('Distance East [m]','d_east', self.db.firstx, self.db.firstx, self.db.nx*self.db.dx-self.db.dx))
-        self.add_parameter(Param('Time fade [s]','tfade', 5.,2.,15))
+        self.add_parameter(Param('Distance [m]','dist', gfdb_maxrange/2, gfdb_minrange, gfdb_maxrange))
+        self.add_parameter(Param('Azimuth [deg]','azi', 0.,-180., 180.))
+        self.add_parameter(Param('Mxx=Myy=Mzz [Nm]','mxx', 1.,-1.,1.))
+        self.add_parameter(Param('Mxy [Nm]','mxy', 0.,-1.,1.))
+        self.add_parameter(Param('Myz [Nm]','myz', 0.,-1.,1.))
+        self.add_parameter(Param('Mxz [Nm]','mxz', 0.,-1.,1.))
+        self.add_parameter(Param('Time fade [s]','tfade', 5.,0.,15))
         self.add_parameter(Switch('Save miniseed traces in .snufflings?','save_mseed', False))
         self.set_live_update(False)
 
@@ -83,16 +108,13 @@ class ParaEditCp_TF_GTTG(extendedSnuffling):
         receivers = []
         station, lat, lon, depth = tab.split()
 
-        d_north=self.d_north
-        d_east=self.d_east
-        origin_lat, origin_lon = orthodrome.ne_to_latlon_alternative_method(float(lat), float(lon), d_north, d_east)
+        origin_lat, origin_lon = lat_lon_from_dist_azi(float(lat), float(lon), self.dist, self.azi)
         r = receiver.Receiver(lat,lon, components='neu', name='.%s.' % station)
         receivers.append(r)
 
         # Composition of the source
         otime = util.str_to_time('2000-01-1 00:00:00')
         db = self.db
-        #db = gfdb.Gfdb('/media/exupery2/gemini-iasp91-20000km/db')
 
         seis = seismosizer.Seismosizer(hosts=['localhost'])
         seis.set_database(db)
@@ -105,18 +127,15 @@ class ParaEditCp_TF_GTTG(extendedSnuffling):
         seis = None
 
         risetime=3
-        mxx=0.
-        myy=0.
-        mzz=0.
-        mxy=0.
-        mxz=-1.
-        myz=0.
+        mxx=self.mxx
+        myy=self.mxx
+        mzz=self.mxx
+        mxy=self.mxy
+        mxz=self.mxz
+        myz=self.myz
         Sourceparams = dict(zip(['mxx', 'myy', 'mzz', 'mxy', 'mxz', 'myz', 'depth', 'rise-time'],
             [mxx, myy, mzz, mxy, mxz, myz,  self.source_depth, risetime]))
         s = source.Source(sourcetype='moment_tensor', sourceparams=Sourceparams)
-        #dip = 90; rake = 0; moment = 7.00e20; depth = 10000; strike=0.
-        #s = source.Source('bilateral',
-        #sourceparams_str='0 0 0 %g %g %g %g %g 0 0 0 0 1 %g' % (depth, moment, strike, dip, rake, risetime))
         self.seis.set_source(s)
         recs = self.seis.get_receivers_snapshot( which_seismograms = ('syn',), which_spectra=(), which_processing='tapered')
         
@@ -129,7 +148,7 @@ class ParaEditCp_TF_GTTG(extendedSnuffling):
         # Define fade in and out, band pass filter and cut off fader for the TF.
         tfade = self.tfade
         freqlimit = (0.005,.006,1,1.2)
-        cut_off_fading = 50
+        cut_off_fading = 500
         ntraces = []
         
         for tr in trs:
@@ -141,7 +160,7 @@ class ParaEditCp_TF_GTTG(extendedSnuffling):
             rename={'e':'BHE','n':'BHN','u':'BHZ'}
             trace_filtered.set_codes(channel=rename[trace_filtered.channel], network='STS2', station='HH', location='syn')
             ntraces.append(trace_filtered)            
-        #self.add_traces(ntraces)        
+        self.add_traces(ntraces)        
 
 def __snufflings__():
     return [ ParaEditCp_TF_GTTG() ]
